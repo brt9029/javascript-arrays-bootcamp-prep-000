dee55edd14fbe8d4c3c63cb763863bf3c2f61e75
Bryant Torres <brt9029@gmail.com>
2018-05-14 16:26:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array
 }
 function addElementToEndOfArray(array, element){
-  array = array.push(element)
+  array.push(element)
   return array
 }
 function destructivelyAddElementToEndOfArray(array, element){
